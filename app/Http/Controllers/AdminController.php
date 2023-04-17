f5c84f581583a6a573532830b94a542fccab0709
EhabElshimi <ehab.developer.email@gmail.com>
2023-04-17 14:39:50 +0200

    add procfile0121

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Contact;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -12,12 +13,13 @@ class AdminController extends Controller
     }
     public function index(){
 
-        $results = DB::table('contacts')
-                        ->select('contacts.id', 'contacts.email', DB::raw('GROUP_CONCAT(phones.number SEPARATOR ", ") AS phone_numbers'),DB::raw('GROUP_CONCAT(social_media.icon SEPARATOR ", ") AS icon'),DB::raw('GROUP_CONCAT(social_media.link SEPARATOR ", ") AS link'))
-                        ->join('phones', 'contacts.id', '=', 'phones.contact_id')
-                        ->join(DB::raw('(SELECT contact_id, GROUP_CONCAT(icon SEPARATOR ", ") AS icon, GROUP_CONCAT(link SEPARATOR ", ") AS link FROM social_media GROUP BY contact_id) AS social_media'), 'contacts.id', '=', 'social_media.contact_id')
-                        ->groupBy('contacts.id', 'contacts.email')
-                        ->get();
+        // $results = DB::table('contacts')
+        //                 ->select('contacts.id', 'contacts.email', DB::raw('GROUP_CONCAT(phones.number SEPARATOR ", ") AS phone_numbers'),DB::raw('GROUP_CONCAT(social_media.icon SEPARATOR ", ") AS icon'),DB::raw('GROUP_CONCAT(social_media.link SEPARATOR ", ") AS link'))
+        //                 ->join('phones', 'contacts.id', '=', 'phones.contact_id')
+        //                 ->join(DB::raw('(SELECT contact_id, GROUP_CONCAT(icon SEPARATOR ", ") AS icon, GROUP_CONCAT(link SEPARATOR ", ") AS link FROM social_media GROUP BY contact_id) AS social_media'), 'contacts.id', '=', 'social_media.contact_id')
+        //                 ->groupBy('contacts.id', 'contacts.email')
+        //                 ->get();
+        $results = Contact::all();
         dd($results);
     }
 
